214003bcaa5754d2f512f9ca61a654beb4c7ee1e
Yoginara Pratama Sitorus <114574855+Yoginara@users.noreply.github.com>
2024-04-26 15:51:27 +0700

    praktikkum

--- js/fetch.js
+++ js/fetch.js
@@ -1,6 +1,11 @@
 import { get } from "https://bukulapak.github.io/api/process.js";
+import { setInner } from "https://bukulapak.github.io/element/process.js";
 let urlAPI = "https://ws-yoginara2024-6b64bd121a73.herokuapp.com/presensi";
 get(urlAPI,isiTablePresensi);
 function isiTablePresensi(results){
     console.log(results);
+    results.forEach(isiRow);
+}
+function isiRow(value){
+    console.log(value)
 }
