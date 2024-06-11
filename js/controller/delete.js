974f2ce85986efff02ff1f415f9a7fab57e0f087
Yoginara Pratama Sitorus <114574855+Yoginara@users.noreply.github.com>
2024-06-11 22:46:37 +0700

    onclick

--- js/controller/delete.js
+++ js/controller/delete.js
@@ -1,17 +1,23 @@
 function deleteData(IDHAPUS) {
-    var presensiId = IDHAPUS;
-    var target_url = "https://ws-yoginara-2024-479b633e2c8b.herokuapp.com/delete/" + presensiId;
+  var presensiId = IDHAPUS;
+  var target_url = 'https://ws-yoginara-2024-479b633e2c8b.herokuapp.com/delete/' + presensiId;
 
-    var requestOptions = {
-        method: 'DELETE',
-        redirect: 'follow'
-    };
+  var requestOptions = {
+    method: 'DELETE',
+    redirect: 'follow',
+  };
 
-    fetch(target_url, requestOptions)
-        .then(response => response.json())
-        .then(result => {
-            alert(result.message);
-            location.reload();
-        })
-        .catch(error => console.log('Error:', error));
+  fetch(target_url, requestOptions)
+    .then((response) => response.json())
+    .then((result) => {
+      alert(result.message);
+      location.reload();
+    })
+    .catch((error) => console.log('Error:', error));
+}
+
+function confirmDelete(IDHAPUS) {
+  if (confirm('Apakah ingin menghapus data ID ' + IDHAPUS + '?')) {
+    deleteData(IDHAPUS);
+  }
 }
